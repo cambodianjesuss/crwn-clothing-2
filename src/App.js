ee875e8ed120527e2bde1a356e8f6e70a5a5edfe
Boi Soth <cambodianjesuss@gmail.com>
2022-11-25 12:01:26 -0800

    setting up homepage sections

--- src/App.js
+++ src/App.js
@@ -1,5 +1,43 @@
 const App = () => {
-  return <div>Testing Webhook</div>;
+  return (
+    <div className="categories-container">
+      <div className="category">
+        {/**<img /> */}
+        <div className="category-body">
+          <h2>Hats</h2>
+          <p>Shot Now</p>
+        </div>
+      </div>
+      <div className="category">
+        {/**<img /> */}
+        <div className="category-body">
+          <h2>Jackets</h2>
+          <p>Shot Now</p>
+        </div>
+      </div>
+      <div className="category">
+        {/**<img /> */}
+        <div className="category-body">
+          <h2>Sneakers</h2>
+          <p>Shot Now</p>
+        </div>
+      </div>
+      <div className="category">
+        {/**<img /> */}
+        <div className="category-body">
+          <h2>Womans</h2>
+          <p>Shot Now</p>
+        </div>
+      </div>
+      <div className="category">
+        {/**<img /> */}
+        <div className="category-body">
+          <h2>Mens</h2>
+          <p>Shot Now</p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default App;
