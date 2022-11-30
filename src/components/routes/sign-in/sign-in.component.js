d8ab0196f818b3fbe2465988f0a04607546cd6df
Boi Soth <cambodianjesuss@gmail.com>
2022-11-30 14:05:56 -0800

    useEffect to redirect, but needed to wrap async response inside of useEffect since I can only return a function.

--- src/components/routes/sign-in/sign-in.component.js
+++ src/components/routes/sign-in/sign-in.component.js
@@ -1,24 +1,33 @@
+import { useEffect } from "react";
+import { getRedirectResult } from "firebase/auth";
+
 import {
+  auth,
   signInWithGooglePopup,
   signInWithGoogleRedirect,
   createUserDocumentFromAuth,
 } from "../../../utils/firebase/firebase.utils";
+import { async } from "@firebase/util";
 
 const SignIn = () => {
+  useEffect(() => {
+    const redirectAsync = async () => {
+      const response = await getRedirectResult(auth);
+      console.log(response);
+    };
+    redirectAsync();
+  }, []);
+
   const logGoogleUser = async () => {
     const { user } = await signInWithGooglePopup();
-    createUserDocumentFromAuth(user);
-  };
-
-  const logGoogleRedirectUser = async () => {
-    const { user } = await signInWithGoogleRedirect();
+    const userDocRef = await createUserDocumentFromAuth(user);
   };
 
   return (
     <div>
       <h1>Sign In</h1>
       <button onClick={logGoogleUser}>Sign in with Google Popup</button>
-      <button onClick={logGoogleRedirectUser}>
+      <button onClick={signInWithGoogleRedirect}>
         Sign in with Google Redirect
       </button>
     </div>
