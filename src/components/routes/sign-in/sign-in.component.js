83a558e1123e6115066439702c05ce2976ea3055
Boi Soth <cambodianjesuss@gmail.com>
2022-11-30 13:31:06 -0800

    setting up redirect for logging in google user

--- src/components/routes/sign-in/sign-in.component.js
+++ src/components/routes/sign-in/sign-in.component.js
@@ -1,5 +1,7 @@
+import { async } from "@firebase/util";
 import {
   signInWithGooglePopup,
+  signInWithGoogleRedirect,
   createUserDocumentFromAuth,
 } from "../../../utils/firebase/firebase.utils";
 
@@ -9,10 +11,17 @@ const SignIn = () => {
     createUserDocumentFromAuth(user);
   };
 
+  const logGoogleRedirectUser = async () => {
+    const { user } = await signInWithGoogleRedirect();
+  };
+
   return (
     <div>
       <h1>Sign In</h1>
       <button onClick={logGoogleUser}>Sign in with Google Popup</button>
+      <button onClick={logGoogleRedirectUser}>
+        Sign in with Google Redirect
+      </button>
     </div>
   );
 };
--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -20,16 +20,20 @@ const firebaseConfig = {
 // Initialize Firebase
 const firebaseApp = initializeApp(firebaseConfig);
 
-const provider = new GoogleAuthProvider();
+const googleProvider = new GoogleAuthProvider();
 
-provider.setCustomParameters({
+googleProvider.setCustomParameters({
   prompt: "select_account",
 });
 
 // Auth
-
 export const auth = getAuth();
-export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
+
+// Sign In Methods
+export const signInWithGooglePopup = () =>
+  signInWithPopup(auth, googleProvider);
+export const signInWithGoogleRedirect = () =>
+  signInWithRedirect(auth, googleProvider);
 
 // DB
 
