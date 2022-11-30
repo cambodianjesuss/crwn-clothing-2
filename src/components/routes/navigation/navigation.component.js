79fd2d2349450a08a257a2bde84653e1782d2a02
Boi Soth <cambodianjesuss@gmail.com>
2022-11-29 18:41:03 -0800

    wrapping navigation in Fragment

--- src/components/routes/navigation/navigation.component.js
+++ src/components/routes/navigation/navigation.component.js
@@ -1,11 +1,14 @@
+import { Fragment } from "react";
 import { Outlet } from "react-router-dom";
 
 const Navigation = () => {
   return (
-    <div>
-      <div>Navigation</div>
+    <Fragment>
+      <div className="navigation">
+        <h1>I am the navigation bar</h1>
+      </div>
       <Outlet />
-    </div>
+    </Fragment>
   );
 };
 
