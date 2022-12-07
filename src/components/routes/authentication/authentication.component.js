62e6081e64a431bba1f551466981aa6898d6b271
Boi Soth <cambodianjesuss@gmail.com>
2022-12-07 13:11:14 -0800

    styling authentication route

--- src/components/routes/authentication/authentication.component.js
+++ src/components/routes/authentication/authentication.component.js
@@ -1,22 +1,12 @@
-import {
-  signInWithGooglePopup,
-  createUserDocumentFromAuth,
-} from "../../../utils/firebase/firebase.utils";
-
 import SignInForm from "../../sign-in-form/sign-in-form.component";
 import SignUpForm from "../../sign-up-form/sign-up-form.component";
+import './authentication.styles.scss'
 
 const Authentication = () => {
-  const logGoogleUser = async () => {
-    const { user } = await signInWithGooglePopup();
-    // eslint-disable-next-line no-unused-vars
-    const userDocRef = await createUserDocumentFromAuth(user);
-  };
 
   return (
-    <div>
-      <SignInForm logGoogleUser={logGoogleUser} />
-      {/*<button onClick={logGoogleUser}>Sign in with Google Popup</button>*/}
+    <div className="authentication-container">
+      <SignInForm />
       <SignUpForm />
     </div>
   );
