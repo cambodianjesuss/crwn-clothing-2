7a37df2fdf283fe26cbcc20c7653c363abc143dc
Boi Soth <cambodianjesuss@gmail.com>
2022-12-13 23:55:28 -0800

    setSports

--- src/context/sports.context.js
+++ src/context/sports.context.js
@@ -2,11 +2,13 @@
 
 import { createContext, useState } from "react";
 
+
 export const SportsContext = createContext();
 
 export const SportsProvider = ({children})=>{
   const [sports, setSports] = useState([]);
   const value = {sports, setSports};
 
+  // setSports
   return <SportsContext.Provider value={value}>{children}</SportsContext.Provider>
 }
