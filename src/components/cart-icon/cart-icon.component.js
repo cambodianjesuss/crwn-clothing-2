db340aaa8994be978c2a6927218b2af198058177
Boi Soth <cambodianjesuss@gmail.com>
2022-12-20 14:48:45 -0800

    Added some padding to the global styles

--- src/components/cart-icon/cart-icon.component.js
+++ src/components/cart-icon/cart-icon.component.js
@@ -10,7 +10,7 @@ const CartIcon = () => {
   return (
     <div className='cart-icon-container'>
       <ShoppingIcon className='shopping-icon' />
-      <span className='item-count'></span>
+      <span className='item-count'>0</span>
     </div>
   )
 }
