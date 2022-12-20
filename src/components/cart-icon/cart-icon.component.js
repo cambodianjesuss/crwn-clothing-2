1338c668f74449449509e34425398b83c50f0f03
Boi Soth <cambodianjesuss@gmail.com>
2022-12-20 14:45:02 -0800

    Imported useContext and ProductsContext into CartIcon to display items

--- src/components/cart-icon/cart-icon.component.js
+++ src/components/cart-icon/cart-icon.component.js
@@ -1,12 +1,16 @@
-import {ReactComponent as ShoppingIcon} from '../../assets/shopping-bag.svg'
+import { useContext } from 'react';
+import { ProductsContext } from '../../context/products.context';
 
+import {ReactComponent as ShoppingIcon} from '../../assets/shopping-bag.svg'
 import './cart-icon.styles.scss';
 
 const CartIcon = () => {
+  const {products} = useContext(ProductsContext)
+  console.log(products.length)
   return (
     <div className='cart-icon-container'>
       <ShoppingIcon className='shopping-icon' />
-      <span className='item-count'>0</span>
+      <span className='item-count'></span>
     </div>
   )
 }
