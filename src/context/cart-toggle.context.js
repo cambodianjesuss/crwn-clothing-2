5d85f49aafe12a8e514a050e802142b748096971
Boi Soth <cambodianjesuss@gmail.com>
2022-12-22 12:17:35 -0800

    Scaffold context and helper function to add items to cart

--- src/context/cart-toggle.context.js
+++ src/context/cart-toggle.context.js
@@ -1,14 +1,27 @@
 import { createContext, useState } from 'react';
 
+// Helper function to check if we need to add a new cart item or update quantity
+const addCartItem = (cartItems, productToAdd) => {
+
+}
+
 export const CartToggleContext = createContext({
   isCartOpen: false,
   setIsCartOpen: ()=>{},
   cartItems: [],
+  addItemToCart: ()=>{}
 });
 
 export const CartToggleProvider = ({children})=> {
 
-  const [isCartOpen, cartItems, setIsCartOpen] = useState(false);
+  const [isCartOpen, setIsCartOpen] = useState(false);
+  const [cartItems, setCartItems] = useState([]);
+
+  const addItemToCart = (productToAdd) => {
+    setCartItems(addCartItem(cartItems, productToAdd));
+  } 
+
+
   const value = { isCartOpen, setIsCartOpen}
 
   return <CartToggleContext.Provider value={value}>{children}</CartToggleContext.Provider>
