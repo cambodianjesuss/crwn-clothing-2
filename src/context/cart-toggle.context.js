7e7853184f2f0fb60e87c65a4e4ca5f04a4b43c7
Boi Soth <cambodianjesuss@gmail.com>
2022-12-21 21:38:48 -0800

    adding toggle to cart

--- src/context/cart-toggle.context.js
+++ src/context/cart-toggle.context.js
@@ -2,12 +2,13 @@ import { createContext, useState } from 'react';
 
 export const CartToggleContext = createContext({
   isCartOpen: false,
-  setIsCartOpen: ()=>{}
+  setIsCartOpen: ()=>{},
+  cartItems: [],
 });
 
 export const CartToggleProvider = ({children})=> {
 
-  const [isCartOpen, setIsCartOpen] = useState(false);
+  const [isCartOpen, cartItems, setIsCartOpen] = useState(false);
   const value = { isCartOpen, setIsCartOpen}
 
   return <CartToggleContext.Provider value={value}>{children}</CartToggleContext.Provider>
