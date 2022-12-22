1ad9a5ad10b7adbb05f702421f181bb86469eaac
Boi Soth <cambodianjesuss@gmail.com>
2022-12-21 21:40:42 -0800

    merging

--- src/components/cart-dropdown/cart-dropdown.component.js
+++ src/components/cart-dropdown/cart-dropdown.component.js
@@ -1,5 +1,6 @@
 // Components
 import Button from '../button/button.component';
+import CartItem from '../cart-item/cart-item.component';
 
 // Assets & Styles
 import './cart-dropdown.styles.scss';
@@ -7,7 +8,9 @@ import './cart-dropdown.styles.scss';
 const CartDropown = () => {
   return (
     <div className='cart-dropdown-container'>
-      <div className='cart-items'></div>
+      <div className='cart-items'>
+        <CartItem />
+      </div>
       <Button>GO TO CHECKOUT</Button>
     </div>
   )
