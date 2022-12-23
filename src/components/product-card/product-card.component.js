17b5384ec2351f2eebaf7364d8e77b9a8252fe75
Boi Soth <cambodianjesuss@gmail.com>
2022-12-22 21:23:04 -0800

    Invoking onClickHandler before passing product to CartContext

--- src/components/product-card/product-card.component.js
+++ src/components/product-card/product-card.component.js
@@ -1,16 +1,32 @@
+import { useContext } from 'react';
+
+// Components
 import Button from '../button/button.component';
+
+// Context
+import { CartContext } from '../../context/cart.context';
+
+// Assets & Styles
 import './product-card.styles.scss'
 
 const ProductCard = ({product}) => {
-  const {name, price, imageUrl} = product;
+  const {name, price, imageUrl, id} = product;
+
+  const { addItemToCart } = useContext(CartContext);
+
+  const onClickHandler = (product) => {
+    console.log(product.id, product.name, product.price)
+  }
+
   return (
     <div className='product-card-container'>
       <img src={imageUrl} alt={`${name}`}/>
       <div className='footer'>
         <span className='name'>{name}</span>
         <span className='price'>{price}</span>
+        <span>{id}</span>
       </div>
-      <Button buttonType="inverted">Add to cart</Button>
+      <Button buttonType="inverted" onClick={()=> onClickHandler(product)}>Add to cart</Button>
     </div>
   )
 }
