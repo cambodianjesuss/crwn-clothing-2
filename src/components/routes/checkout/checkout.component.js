984e58cc6ff5880bdc98cdbfd2178abd61b20d6f
Boi Soth <cambodianjesuss@gmail.com>
2022-12-24 00:32:12 -0800

    Using CartContext to map the cartItems on the checkout page

--- src/components/routes/checkout/checkout.component.js
+++ src/components/routes/checkout/checkout.component.js
@@ -1,10 +1,31 @@
+import { useContext } from 'react';
+
+// Context
+import { CartContext } from '../../../context/cart.context';
+
 // Assets & Styles
 import './checkout.styles.scss'
 
 const Checkout = () => {
+
+  const { cartItems } = useContext(CartContext)
   return (
     <div>
       <h1>I am the checkout page</h1>
+      <div>
+        {
+          cartItems.map((cartItem)=>{
+
+            const { id, name, quantity } = cartItem;
+            return (
+              <div key={id}>
+                <h2>{name}</h2>
+                <span>{quantity}</span>
+              </div>
+            )
+          })
+        }
+      </div>
     </div>
   )
 }
