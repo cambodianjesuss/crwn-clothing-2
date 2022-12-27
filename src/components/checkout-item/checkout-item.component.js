4292ad1aa36f9364a07887d7c7d76a661dc62083
Boi Soth <cambodianjesuss@gmail.com>
2022-12-27 00:04:06 -0800

    Added functionality to checkout page quantity arrows to increment or decrement product's quantity

--- src/components/checkout-item/checkout-item.component.js
+++ src/components/checkout-item/checkout-item.component.js
@@ -8,9 +8,16 @@ import './checkout-item.styles.scss'
 
 const CheckoutItem = ({cartItem})=> {
   
-  const { clearItemFromCart } = useContext(CartContext);
+  const { addItemToCart, removeItemFromCart, clearItemFromCart } = useContext(CartContext);
 
   const clearItemHandler = () => clearItemFromCart(cartItem);
+
+  const addItemHandler = () => {
+    addItemToCart(cartItem)
+  }
+  const removeItemHandler = () => {
+    removeItemFromCart(cartItem)
+  }
 
   const { name, imageUrl, price, quantity} = cartItem;
 
@@ -20,7 +27,15 @@ const CheckoutItem = ({cartItem})=> {
         <img src={imageUrl} alt={`${name}`} />
       </div>
       <span className='name'>{name}</span>
-      <span className='quantity'>{quantity}</span>
+      <span className='quantity'>
+        <div className='arrow' onClick={removeItemHandler}>
+          &#10094;
+        </div>
+        <span className='value'>{quantity}</span>
+        <div className='arrow' onClick={addItemHandler}>
+          &#10095;
+        </div>
+      </span>
       <span className='price'>${price}</span>
       <div className='remove-button' onClick={clearItemHandler}> &#10005;</div>
     </div>
