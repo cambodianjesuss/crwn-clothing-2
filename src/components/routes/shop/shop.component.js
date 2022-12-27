4d98983c3ca23b08d6345ee00b6549b7b04df94b
Boi Soth <cambodianjesuss@gmail.com>
2022-12-27 13:56:31 -0800

    key prop fix

--- src/components/routes/shop/shop.component.js
+++ src/components/routes/shop/shop.component.js
@@ -12,24 +12,20 @@ import './shop.styles.scss'
 const Shop = ()=>{
   const {categoriesMap} = useContext(CategoriesContext)
   console.log(categoriesMap)
+
+
   return (
     <Fragment>
-      {
-        Object.keys(categoriesMap).map(title => {
-          return (
-            <Fragment key={title.id}>
-            <h2>{title}</h2>
-            <div className="products-container">
-              {
-                categoriesMap[title].map((product)=>(
+      { Object.keys(categoriesMap).map(title => (
+        <Fragment key={title}>
+          <h2>{title.toLocaleUpperCase()}</h2>
+          <div className="products-container">
+            {categoriesMap[title].map((product)=>(
                   <ProductCard key={product.id} product={product}/>
-                ))
-              }
-            </div>
-          </Fragment>
-          )
-        })
-      }
+            ))}
+          </div>
+        </Fragment>
+      ))}
     </Fragment>
   )
 }
