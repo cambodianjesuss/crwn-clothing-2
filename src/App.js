efceba5cd42048213996a99958d9b0c3ace3bb94
Boi Soth <cambodianjesuss@gmail.com>
2023-01-07 17:20:00 -0800

    rendering nested routes for Shop components now

--- src/App.js
+++ src/App.js
@@ -6,25 +6,15 @@ import Home from "./components/routes/home/home.component";
 import Authentication from "./components/routes/authentication/authentication.component";
 import Shop from "./components/routes/shop/shop.component";
 import Checkout from "./components/routes/checkout/checkout.component";
-import Hats from "./components/routes/hats/hats.component";
-import Jackets from "./components/routes/jackets/jackets.component";
-import Sneakers from "./components/routes/sneakers/sneakers.component";
-import Womens from "./components/routes/womens/womans.component";
-import Mens from "./components/routes/mens/mens.component";
 
 const App = () => {
   return (
     <Routes>
       <Route path="/" element={<Navigation />}>
         <Route index element={<Home />} />
-        <Route path="shop" element={<Shop />} />'
+        <Route path="shop/*" element={<Shop />} />'
         <Route path="auth" element={<Authentication />} />
         <Route path="checkout" element={<Checkout />} />
-        <Route path="hats" element={<Hats />} />
-        <Route path="jackets" element={<Jackets />} />
-        <Route path="sneakers" element={<Sneakers />} />
-        <Route path="womens" element={<Womens />} />
-        <Route path="mens" element={<Mens />} />
       </Route>
     </Routes>
   );
--- src/components/routes/shop/shop.component.js
+++ src/components/routes/shop/shop.component.js
@@ -1,28 +1,16 @@
-import { useContext, Fragment } from "react";
-import { Link } from "react-router-dom";
+import { Routes, Route} from 'react-router-dom';
 
 // Components
-import CategoryPreview from "../../category-preview/category-preview.component";
-
-
-// Context
-import { CategoriesContext } from "../../../context/categories.context";
+import CategoriesPreview from '../categories-preview/categories-preview.component';
 
 // Assets Styles
 import './shop.styles.scss'
 
 const Shop = ()=>{
-  const {categoriesMap} = useContext(CategoriesContext)
-  // Categories map pulls from Categories Context from Firebase
-
-
   return (
-    <div className="shop-container">
-      { Object.keys(categoriesMap).map(title => {
-        const products = categoriesMap[title];
-        return <CategoryPreview key={title} title={title} products={products}/>
-      })}
-    </div>
+    <Routes>
+      <Route index element={<CategoriesPreview />}/>
+    </Routes>
   )
 }
 
